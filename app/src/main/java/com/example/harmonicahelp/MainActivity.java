eebd016e1a511ccac0769c574f55aa2346a28594
AndrianAD <druc_andrian@mail.ru>
2017-06-23 11:08:13 +0300

    170623

--- app/src/main/java/com/example/harmonicahelp/MainActivity.java
+++ app/src/main/java/com/example/harmonicahelp/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends Activity {
     private static ArrayList noteList2 = new ArrayList();
     private static ArrayList noteList = new ArrayList();
     private static ArrayList tempArray = new ArrayList();
-    Note[] note = Note.values;
+    Note[] note;
     public static int n = 5;
     public static int z = 0;
     static int octava_set = 0;
@@ -41,6 +41,7 @@ public class MainActivity extends Activity {
     CustomKeyboard mCustomKeyboard;
     ClipboardManager clipboardManager;
     ClipData clipData;
+    int stroi=3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +63,58 @@ public class MainActivity extends Activity {
 
         mCustomKeyboard = new CustomKeyboard(this, R.id.keyboardview, R.xml.hexkbd);
         mCustomKeyboard.registerEditText(R.id.edit_text_enter_tabl);
+
+
+        if(stroi==1) {
+            note= new Note[]{
+                    new Note("G", "1"), new Note("Ab", "-1'"), new Note("A", "-1"), new Note("Bb", "1*"),
+                    new Note("B", "2"), new Note("C", "-2''"), new Note("C#", "-2'"), new Note("D", "-2"),
+                    new Note("Eb", "-3'''"), new Note("E", "-3''"), new Note("F", "-3'"), new Note("F#", "-3"),
+                    new Note("G", "4"), new Note("Ab", "-4'"), new Note("A", "-4"), new Note("Bb", "4*"),
+                    new Note("B", "5"), new Note("C", "-5"), new Note("C#", "5*"), new Note("D", "6"),
+                    new Note("Eb", "-6'"), new Note("E", "-6"), new Note("F", "6*"), new Note("F#", "-7"),
+                    new Note("G", "7"), new Note("Ab", "-7*"), new Note("A", "-8"), new Note("Bb", "8'"),
+                    new Note("B", "8"), new Note("C", "-9"), new Note("C#", "9'"), new Note("D", "9"),
+                    new Note("Eb", "-9*"), new Note("E", "-10"), new Note("F", "10''"), new Note("F#", "10'"),
+                    new Note("G", "10"), new Note("Ab", "10*"), new Note("A", ""), new Note("Bb", ""),
+                    new Note("B", ""), new Note("C", ""), new Note("C#", ""), new Note("D", ""), new Note("Eb", ""),
+                    new Note("E", ""), new Note("F", ""), new Note("F#", ""), new Note("G", "")};
+        }
+        if(stroi==2) {
+            note= new Note[]{
+                    new Note("G", "1"), new Note("Ab", "-1'"), new Note("A", "-1"), new Note("Bb", "1*"),
+                    new Note("B", "2"), new Note("C", "-2''"), new Note("C#", "-2'"), new Note("D", "-2"),
+                    new Note("Eb", "-3*"), new Note("E", "+3"), new Note("F", "-3'"), new Note("F#", "-3"),
+                    new Note("G", "4"), new Note("Ab", "-4'"), new Note("A", "-4"), new Note("Bb", "4*"),
+                    new Note("B", "5"), new Note("C", "-5"), new Note("C#", "5*"), new Note("D", "6"),
+                    new Note("Eb", "-6'"), new Note("E", "-6"), new Note("F", "6*"), new Note("F#", "-7"),
+                    new Note("G", "7"), new Note("Ab", "-7*"), new Note("A", "-8"), new Note("Bb", "8'"),
+                    new Note("B", "8"), new Note("C", "-9"), new Note("C#", "9'"), new Note("D", "9"),
+                    new Note("Eb", "-9*"), new Note("E", "-10"), new Note("F", "10''"), new Note("F#", "10'"),
+                    new Note("G", "10"), new Note("Ab", "10*"), new Note("A", ""), new Note("Bb", ""),
+                    new Note("B", ""), new Note("C", ""), new Note("C#", ""), new Note("D", ""), new Note("Eb", ""),
+                    new Note("E", ""), new Note("F", ""), new Note("F#", ""), new Note("G", "")};
+        }
+
+        if(stroi==3) {
+            note= new Note[]{
+                    new Note("G", "1"), new Note("Ab", "-1'"), new Note("A", "-1"), new Note("Bb", "1*"),
+                    new Note("B", "2"), new Note("C", "-2''"), new Note("C#", "-2'"), new Note("D", "-2"),
+                    new Note("Eb", "-3'''"), new Note("E", "-3''"), new Note("F", "-3'"), new Note("F#", "-3"),
+                    new Note("G", "4"), new Note("Ab", "-4'"), new Note("A", "-4"), new Note("Bb", "4*"),
+                    new Note("B", "5"), new Note("C", "-5'"), new Note("C#", "-5"), new Note("D", "6"),
+                    new Note("Eb", "-6'"), new Note("E", "-6"), new Note("F", "6*"), new Note("F#", "-7"),
+                    new Note("G", "7"), new Note("Ab", "-7*"), new Note("A", "-8"), new Note("Bb", "8'"),
+                    new Note("B", "8"), new Note("C", "-9"), new Note("C#", "9'"), new Note("D", "9"),
+                    new Note("Eb", "-9*"), new Note("E", "-10"), new Note("F", "10''"), new Note("F#", "10'"),
+                    new Note("G", "10"), new Note("Ab", "10*"), new Note("A", ""), new Note("Bb", ""),
+                    new Note("B", ""), new Note("C", ""), new Note("C#", ""), new Note("D", ""), new Note("Eb", ""),
+                    new Note("E", ""), new Note("F", ""), new Note("F#", ""), new Note("G", "")};
+        }
+
+
+
+
 
 
         Button reset = (Button) findViewById(R.id.reset_id);
--- app/src/main/java/com/example/harmonicahelp/Note.java
+++ app/src/main/java/com/example/harmonicahelp/Note.java
@@ -1,19 +1,5 @@
 package com.example.harmonicahelp;
 class Note {
-    public static Note[] values = {
-            new Note("G", "1"), new Note("Ab", "-1'"), new Note("A", "-1"), new Note("Bb", "1*"),
-            new Note("B", "2"), new Note("C", "-2''"), new Note("C#", "-2'"), new Note("D", "-2"),
-            new Note("Eb", "-3'''"), new Note("E", "-3''"), new Note("F", "-3'"), new Note("F#", "-3"),
-            new Note("G", "4"), new Note("Ab", "-4'"), new Note("A", "-4"), new Note("Bb", "4*"),
-            new Note("B", "5"), new Note("C", "-5"), new Note("C#", "5*"), new Note("D", "6"),
-            new Note("Eb", "-6'"), new Note("E", "-6"), new Note("F", "6*"), new Note("F#", "-7"),
-            new Note("G", "7"), new Note("Ab", "-7*"), new Note("A", "-8"), new Note("Bb", "8'"),
-            new Note("B", "8"), new Note("C", "-9"), new Note("C#", "9'"), new Note("D", "9"),
-            new Note("Eb", "-9*"), new Note("E", "-10"), new Note("F", "10''"), new Note("F#", "10'"),
-            new Note("G", "10"), new Note("Ab", "10*"), new Note("A", ""), new Note("Bb", ""),
-            new Note("B", ""), new Note("C", ""), new Note("C#", ""), new Note("D", ""), new Note("Eb", ""),
-            new Note("E", ""), new Note("F", ""), new Note("F#", ""), new Note("G", "")};
-
     private String nota;
     private String tab;
 
